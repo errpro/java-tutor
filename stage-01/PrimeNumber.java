48430deccc40335c9fc90265b3bdaa78f47193dc
oxnz <yunxinyi@gmail.com>
2014-10-11 15:36:37 +0800

    complete PrimeNumber.java

--- stage-01/PrimeNumber.java
+++ stage-01/PrimeNumber.java
@@ -4,7 +4,7 @@
 
       Version: 0.1
       Created: 2014-10-11 14:13:55
-  Last-update: 2014-10-11 14:14:21
+  Last-update: 2014-10-11 15:35:47
      Revision: None
 
        Author: Oxnz
@@ -15,4 +15,20 @@ Revision history:
 */
 
 public class PrimeNumber {
-	public void list(int begin, int end)
+	public void list(int begin, int end) {
+		int j;
+		for (int i = begin; i < end; ++i) {
+			for (j = 2; j < i; ++j) {
+				if (i % j == 0)
+					break;
+			}
+			if (j == i)
+				System.out.println(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		PrimeNumber primeNum = new PrimeNumber();
+		primeNum.list(2, 100);
+	}
+}
